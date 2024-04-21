cc3561da4c6b2926ebdefacf2885fe3ac2f4be9e
Abdul Waheed Desktop home <bigbhatti586@gmail.com>
2024-04-21 13:45:55 +0500

    simulating with datamem attached in testbench, working. use reg (x12) for data display.

--- Datapath/mem_data.sv
+++ Datapath/mem_data.sv
@@ -1,11 +1,13 @@
-module mem_data (input logic clk, input logic [31:0] address, data_in, input logic rd_wr_en, bus_cs,input logic [3:0]mask, output logic [31:0]data_out, output logic valid, output logic [31:0] led_display);
+module mem_data (input logic clk, input logic [31:0] address, data_in, input logic rd_wr_en, bus_cs,input logic [3:0]mask, output logic [31:0]data_out, output logic valid);
 	logic [31:0]Data_mem [1024:0];
 	initial begin 
 	$readmemh ("C:/Users/Prince/Desktop/rv32_for_fyp/Script/build/DECACHE.mem",Data_mem);
             end
+
+	logic valid_pre_flip;
 	always_ff@(posedge clk) begin //read operation
-	valid =1'b0;
-	if (rd_wr_en & ~bus_cs)	begin 
+	valid_pre_flip =1'b0;
+	if (~rd_wr_en & bus_cs)	begin 
 			/*if (mask[0]) begin 
 				data_out[7:0] = Data_mem[address[31:2]][7:0];
 				end
@@ -19,13 +21,13 @@ module mem_data (input logic clk, input logic [31:0] address, data_in, input log
 				data_out[31:24] = Data_mem[address[31:2]][31:24];
 				end*/
 		data_out=Data_mem[address[31:2]];
-		valid =1'b1;
+		valid_pre_flip =1'b1;
 			end
 		end
 
 
 	always_ff@(negedge clk) begin //write operation
-	if (~rd_wr_en & ~bus_cs)	begin 
+	if (rd_wr_en & bus_cs)	begin 
 			if (mask[0]) begin 
 				 Data_mem[address[31:2]][7:0]<=data_in[7:0];
 				end
@@ -43,5 +45,9 @@ module mem_data (input logic clk, input logic [31:0] address, data_in, input log
 			end
 		end
 
-	assign  led_display = Data_mem[0][31:0];
+		always_ff @( posedge clk ) begin 
+			valid <=valid_pre_flip;
+		end
+
+	
 endmodule
