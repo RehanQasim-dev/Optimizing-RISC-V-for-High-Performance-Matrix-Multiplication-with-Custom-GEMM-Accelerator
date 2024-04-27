a27e5ab5d5df19e1a19ba6195a0e2d1032f2812f
Abdul Waheed Desktop home <bigbhatti586@gmail.com>
2024-04-27 17:43:49 +0500

    made a maketxt4.py file which stores data into all banks. we take the first input and store into bank 0 index 0, second data bank1 index 0 and so on untill we hit the end of banks. then we write to bank 0 index 1 and so on.
    to do this we wrote a file that takes the binary input and writes it as
    a string to be concatinated if teh banks run out. i.e. if i have data
    that is to be stores to bank 0 index 1, i will write to the entire bank
    0 as 0x0104 where 04 is data at bank 0 index 0 and 01 is data at bank 0
    index 1.  thus we can populate the data memory of any no of data
    elements

---
+++ Script/maketxt4.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+import sys
+
+binfile = sys.argv[1]
+# binfile = "C:/Users/Prince/Desktop/rv32_for_fyp/Script/build/DCACHE.bin"
+
+with open(binfile, "rb") as f:
+    bindata = f.read()
+
+# Pad the data with zeros to make it evenly divisible by 16
+remainder = len(bindata) % 16
+if remainder != 0:
+    bindata += b'\x00' * (16 - remainder)
+
+# Calculate the number of words (assuming each word is 16 bytes)
+nwords = len(bindata) // 16
+
+# Initialize lists to store data for each bank
+bank_data = [""] * 16
+
+# Iterate over the words in the binary file
+for i in range(nwords):
+    # Extract 16 bytes (one word)
+    w = bindata[16 * i : 16 * (i + 1)]
+    # Iterate over each byte in the word
+    for j in range(16):
+        # Append the byte as a hexadecimal string to the corresponding bank's data
+        bank_data[j] = "%02x" % w[j] + bank_data[j]
+
+# Iterate over the bank data and print the result
+for i, bank in enumerate(bank_data):
+    print("@%01x" % i, bank)
