abae65e15f46c0f756dd202df31b4a6ded6cec73
Rehan Qasim <rehanbhatti0317@gmail.com>
2024-05-25 02:04:54 +0500

    Added new matrix print function, only smaller matrices could be receuved for now

--- test.py
+++ test.py
@@ -1,27 +1,7 @@
 import numpy as np
 
-# Define the matrices A and B
-A = np.array([
-    [-16, -102, 3],
-    [49, 53, -67]
-])
+# Declare a 40x40 matrix filled with zeros
+matrix = np.zeros((40, 40))
 
-B = np.array([
-    [76, 8],
-    [99, -72],
-    [-101, 12]
-])
-
-# Initialize the result matrix C with zeros
-# C = np.zeros((2, 2), dtype=np.int8)
-
-# # Perform matrix multiplication
-# for i in range(2):
-#     for j in range(2):
-#         for k in range(3):
-#             C[i][j] += A[i][k] * B[k][j]
-
-C=np.dot(A,B)
-# Print the result matrix C
-print("Result of A * B:")
-print(C)
+# Print the matrix
+print(matrix)
